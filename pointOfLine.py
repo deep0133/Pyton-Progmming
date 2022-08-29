c1a9458b3b256612d4e73097a972d719acca2c14
deep0133 <deepanshuheer27@gmail.com>
2022-08-29 20:45:04 +0530

    Bresenham's Line Drawing Algorithm -> only points calculating

---
+++ pointOfLine.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+
+# variables declare:
+
+print("Enter the value of x1 : ")
+x1 = int(input())
+
+print("Enter the value of y1 : ")
+y1 = int(input())
+
+print("Enter the value of xn : ")
+xn = int(input())
+
+print("Enter the value of yn : ")
+yn = int(input())
+
+# Calculating value of delta-X and delta-Y:
+deltaY = yn - y1
+deltaX = xn - x1
+
+# Calculate slop of line:
+slope = deltaY / deltaX
+
+# Decision Parameter P:
+p = 2*deltaY - deltaX
+
+# Initial point of a line:
+print("(",x1,",",y1,") ")
+
+#  checking slope condition:
+if slope < 1 :
+        
+    while x1 < xn :
+
+        if p < 0 :
+            x1 = x1 + 1
+            y1 = y1
+            p = p + 2*deltaY
+            print("(",x1,",",y1,") ")
+        else :
+            x1 = x1 + 1
+            y1 = y1 + 1
+            p = p + 2*deltaY - 2*deltaX
+            print("(",x1,",",y1,") ")
+
+
+else :
+     while x1 < xn :
+
+        if p < 0 :
+            x1 = x1 
+            y1 = y1 + 1
+            p = p + 2*deltaX
+            print("(",x1,",",y1,") ")
+        else :
+            x1 = x1 + 1
+            y1 = y1 + 1
+            p = p + 2*deltaX - deltaY
+            print("(",x1,",",y1,") ")
+
